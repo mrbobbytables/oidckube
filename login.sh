044d198ba0d4458844742bdebc9ba56c306d1608
Bob Killen <rkillen@umich.edu>
2018-05-27 15:57:10 -0400

    initial commit

---
+++ login.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[[ ! -f "$DIR/config" ]] && cp "$DIR/config.example" "$DIR/config" 
+
+# shellcheck source=config
+source "$DIR/config"
+
+JQ_RELEASE="1.5"
+BIN_DIR="$DIR/bin"
+PATH=$PATH:$BIN_DIR
+
+install_jq() {
+ command -v jq >/dev/null 2>&1 && return 0
+  echo "[$(date)][WARNING] jq not found in path. Attempting to fetch."
+  local jq_url="https://github.com/stedolan/jq/releases/download/jq-$JQ_RELEASE/"
+  if echo "$OSTYPE" | grep -q "darwin"; then
+    jq_url+="jq-osx-amd64"
+  elif echo "$OSTYPE" | grep -q "linux"; then
+    jq_url+="jq-linux64"
+  else
+    echo "[$(date)][ERROR] Unsupported OS for dependency resolution. Please install jq manually."
+    return 1
+  fi
+  curl -SL "$jq_url" -o "$BIN_DIR/jq"
+  chmod +x "$BIN_DIR/jq"
+}
+
+get_creds() {
+  echo "Please input your credentials for https://$KEYCLOAK_ADDRESS/auth/realms/$KEYCLOAK_AUTH_REALM"
+  if [ "$KEYCLOAK_USERNAME" = "" ];then
+	  read -rp "user: " KEYCLOAK_USERNAME
+  fi
+  if [ "$KEYCLOAK_PASSWORD" = "" ];then
+	  read -rsp "pass: " KEYCLOAK_PASSWORD
+    echo
+  fi
+}
+
+get_token() {
+  local keycloak_token_url="https://$KEYCLOAK_ADDRESS/auth/realms/$KEYCLOAK_AUTH_REALM/protocol/openid-connect/token"
+  echo "[$(date)][INFO] Requesting token from $keycloak_token_url"
+  
+  TOKEN=$(curl -k -s "$keycloak_token_url" \
+    -d grant_type=password \
+    -d response_type=id_token \
+    -d scope=openid \
+    -d client_id="$KEYCLOAK_CLIENT_ID" \
+    -d client_secret="$KEYCLOAK_CLIENT_SECRET" \
+    -d username="$KEYCLOAK_USERNAME" \
+    -d password="$KEYCLOAK_PASSWORD")
+
+  ERROR=$(echo "$TOKEN" | jq .error -r)
+  if [ "$ERROR" != "null" ];then
+	  echo "[$(date)][ERROR]  $TOKEN" >&2
+	  return 1
+  fi
+}
+
+set_creds() {
+  local id_token refresh_token
+  id_token=$(echo "$TOKEN" | jq .id_token -r)
+  refresh_token=$(echo "$TOKEN" | jq .refresh_token -r)
+  
+  echo "[$(date)][INFO] Adding user $KEYCLOAK_USERNAME to kube config"
+  
+  kubectl config set-credentials "$KEYCLOAK_USERNAME" \
+    --auth-provider=oidc \
+    --auth-provider-arg=idp-certificate-authority="$DIR/pki/keycloak-ca.pem" \
+    --auth-provider-arg=idp-issuer-url="https://$KEYCLOAK_ADDRESS/auth/realms/$KEYCLOAK_AUTH_REALM" \
+  	--auth-provider-arg=client-id="$KEYCLOAK_CLIENT_ID" \
+  	--auth-provider-arg=client-secret="$KEYCLOAK_CLIENT_SECRET" \
+  	--auth-provider-arg=id-token="$id_token" \
+  	--auth-provider-arg=refresh-token="$refresh_token"
+}
+
+main() {
+  install_jq
+  get_creds
+  get_token
+  set_creds
+}
+
+main "$@"
---
+++ oidckube.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+[[ ! -f "$DIR/config" ]] && cp "$DIR/config.example" "$DIR/config" 
+# shellcheck source=config
+source "$DIR/config"
+
+CFSSL_RELEASE="1.2"
+BIN_DIR="$DIR/bin"
+PKI_DIR="$DIR/pki"
+PKI_PROFILE_DIR="$DIR/pki-profiles"
+TEMPLATE_DIR="$DIR/templates"
+MANIFEST_DIR="$DIR/manifests"
+PATH=$PATH:$BIN_DIR
+
+install_cfssl() {
+  command -v cfssljson >/dev/null 2>&1 && { echo "[$(date)][INFO] cfssl found."; return 0; }
+  echo "[$(date)][WARNING] cfssl and cfssljson not found in path."
+  if echo "$OSTYPE" | grep -q "darwin"; then
+    echo "[$(date)][ERROR] cfssl not found. Please install with brew or 'go-get' See: https://github.com/cloudflare/cfssl/issues/813 for more information."
+    return 1
+  elif echo "$OSTYPE" | grep -q "linux"; then
+    curl -SL "https://pkg.cfssl.org/R$CFSSL_RELEASE/cfssl_linux-amd64" -o "$BIN_DIR/cfssl"
+    curl -SL "https://pkg.cfssl.org/R$CFSSL_RELEASE/cfssljson_linux-amd64" -o "$BIN_DIR/cfssljson"
+    chmod +x "$BIN_DIR/"{cfssl,cfssljson}
+  else
+    echo "[$(date)][ERROR] Unsupported OS for dependency resolution. Please install cfssl manually."
+    return 1
+  fi
+}
+
+init_pki() {
+  echo "[$(date)][INFO] Generating Certificates."
+  sed -e "s|__KEYCLOAK_ADDRESS__|$KEYCLOAK_ADDRESS|g" \
+  "$TEMPLATE_DIR/keycloak.json.tmplt" > "$PKI_PROFILE_DIR/keycloak.json"
+
+  cfssl gencert -initca "$PKI_PROFILE_DIR/ca-csr.json" | cfssljson -bare "$PKI_DIR/keycloak-ca" -
+  cfssl gencert \
+    -ca="$PKI_DIR/keycloak-ca.pem" \
+    -ca-key="$PKI_DIR/keycloak-ca-key.pem" \
+    -config="$PKI_PROFILE_DIR/ca-config.json" \
+    -profile=server \
+    "$PKI_PROFILE_DIR/keycloak.json" | cfssljson -bare "$PKI_DIR/keycloak"
+}
+
+init_minikube() {
+  echo "[$(date)][INFO] Initializing minikube."
+  minikube start
+  minikube addons enable ingress
+  inject_keycloak_certs
+  init_keycloak
+  local instance_ip
+  instance_ip="$(minikube ip)"
+  while   [ "$(kubectl get statefulset keycloak --template='{{.status.readyReplicas}}')" != "1" ]; do
+     echo "[$(date)][INFO] Waiting for Keycloak to become ready."
+     sleep 10
+  done
+  echo "[$(date)][INFO] Keycloak Ready."
+  echo "[$(date)][INFO] Shutting down minikube."
+  minikube stop
+  VBoxManage modifyvm minikube --natdnshostresolver1 on
+  echo "[$(date)][INFO] Add entry in /etc/hosts file before starting."
+  echo "[$(date)][INFO] $instance_ip  $KEYCLOAK_ADDRESS"
+}
+
+inject_keycloak_certs() {
+  tar -c -C "$PKI_DIR" keycloak-ca.pem | ssh -t -q -o StrictHostKeyChecking=no \
+    -i "$(minikube ssh-key)" "docker@$(minikube ip)" 'sudo tar -x --no-same-owner -C /var/lib/localkube/certs'
+
+}
+
+init_keycloak() {
+  sed -e "s|__KEYCLOAK_ADDRESS__|$KEYCLOAK_ADDRESS|g" \
+  "$TEMPLATE_DIR/ing-keycloak.yaml.tmplt" > "$MANIFEST_DIR/ing-keycloak.yaml"
+  kubectl create secret tls keycloak-cert --cert="$PKI_DIR/keycloak.pem" --key="$PKI_DIR/keycloak-key.pem"
+  kubectl create -f manifests/
+}
+
+start_minikube() {
+  minikube start \
+  --extra-config=kubelet.serialize-image-pulls=false \
+  --extra-config=apiserver.oidc-issuer-url=https://$KEYCLOAK_ADDRESS/auth/realms/$KEYCLOAK_AUTH_REALM \
+  --extra-config=apiserver.oidc-client-id=$KEYCLOAK_CLIENT_ID \
+  --extra-config=apiserver.oidc-username-claim=email \
+  --extra-config=apiserver.oidc-username-prefix="oidc:" \
+  --extra-config=apiserver.oidc-groups-claim=groups \
+  --extra-config=apiserver.oidc-groups-prefix="oidc:" \
+  --extra-config=apiserver.oidc-ca-file=/var/lib/localkube/certs/keycloak-ca.pem
+}
+
+main() {
+  case "$1" in
+    "init")
+      install_cfssl
+      init_pki
+      init_minikube
+      ;;
+    "start")
+      start_minikube
+      ;;
+    *)
+      minikube "$@"
+      ;;
+  esac
+}
+
+main "$@"
